3614030c078fc88b1c952a2052671a4dc7368638
Gabriel Galdino <66391286+gabogaldino@users.noreply.github.com>
2023-09-06 12:46:49 -0300

    Update auth.php

--- App/auth.php
+++ App/auth.php
@@ -3,16 +3,20 @@ session_start();
 
 if(!isset($_SESSION["idUsuario"]) || !isset($_SESSION["usuario"])){
 
-    header('Location: ../'); //Redirecionar para a página de login
+    header('Location: ../'); 
 }else{
+
     $idUsuario = $_SESSION["idUsuario"]; 
     $username   = $_SESSION["usuario"];
     $perm	   = $_SESSION["perm"];
     $foto      = $_SESSION["foto"];
+    $userAgent = $_SERVER["HTTP_USER_AGENT"];
+    $userIP = $_SERVER["REMOTE_ADDR"];
 
     if($perm != "admin"){
         header('Location: index.php');
     }
 
 }
+
 ?>
